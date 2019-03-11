e6c8ec28d99efb906926e9837798fec447695701
liuChongyang95 <18201104668@163.com>
2019-03-11 15:08:59 +0800

    对称二叉树 注意：不能用root.left 和root.right 直接比较。 可能是连带除了根之外的整个分支结构tree进行比较，而非单独一个node。

---
+++ src/main/java/MainTree.java
@@ -0,0 +1,67 @@
+/**
+ * 对称二叉树
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ *     1
+ *   /  \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * 说明:
+ * <p>
+ * 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+ */
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class MainTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode leftRoot, TreeNode rightRoot) {
+        if (leftRoot == null && rightRoot == null) {
+            return true;
+        }
+        if (leftRoot != null && rightRoot == null || leftRoot == null && rightRoot != null || rightRoot.val != leftRoot.val) {
+            return false;
+        }
+        return isSymmetric(rightRoot.left, leftRoot.right) && isSymmetric(rightRoot.right, leftRoot.left);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
